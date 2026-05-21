92b348a148f20ed9e88a026b65fbd00365d5fcfa
Sanskar Sainik <sanskarsainik40@gmail.com>
2026-05-21 18:34:03 +0530

    fixed flake8 issues

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
-print("Hello for my github learn folder")
+def hello():
+    """
+    This is a demo function to test the pre-commit hook.
+
+    It checks for syntax and style issues before allowing
+    the commit to go through.
+    """
+    print("Hello World")
+
+
+hello()
